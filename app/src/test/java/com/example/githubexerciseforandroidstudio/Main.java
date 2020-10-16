33806f1befd108fb21f9e99b0abdbe2895819bb8
Harukimoriwaki <s1871118@s.konasn-u.ac.jp>
2020-10-16 17:01:02 +0900

    名前変えました

--- app/src/test/java/com/example/githubexerciseforandroidstudio/Main.java
+++ app/src/test/java/com/example/githubexerciseforandroidstudio/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("岡野暖大です。");
         System.out.println("岩元健悟です。");
         System.out.println("平瀬奈緒です。");
+        System.out.println("森脇遼輝です。"); 
     }
 }
